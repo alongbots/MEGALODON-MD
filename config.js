4530a32e0053ae84151901b871da40ce5c2f9817
alongbots <aosiblings10@gmail.com>
2025-08-21 20:26:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,17 @@ module.exports = {
     ANTI_DELETE: process.env.ANTI_DELETE || "true",
     ANTI_CALL: process.env.ANTI_CALL || "true",
 
-    REJECT_MSG: process.env.REJECT_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
+    REJECT_MSG: process.env.REJECT_MSG || "*📞 🤖 BERU BOT SYSTEM 🤖  
+━━━━━━━━━━━━━━━━━━━  
+⚡ LORD ALONG is currently busy...  
+📩 Please leave your message after the beep.  
+
+[✔] Your message will be logged.  
+[✔] He will respond when available.  
+
+💠 Stay patient, human... the Lord is watching. 💠  
+━━━━━━━━━━━━━━━━━━━  
+*",
     ANTI_BAD: process.env.ANTI_BAD || "false",                  // Block bad words?
 
     ANTI_LINK_KICK: process.env.ANTILINK_KICK || "false",
